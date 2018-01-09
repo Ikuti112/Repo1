ca476672cef51fb0a3685b463bb86c46d89c5522
molkiewicz <Borgitek@gmail.com>
2018-01-10 00:11:02 +0100

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] tmp;
+            int m = 47527;
+            int[] T = new int[m];
+            int[] wartosci = new int[3744];
+            string path = @"/home/Dave/Downloads";
+            int k = 0;
+            int wartosc = 0;
+            tmp = File.ReadAllLines(path);
+            int h;
+            foreach(string a in tmp)
+            {
+                for(int i = 0; i < a.Length; i++)
+                {
+                    wartosc += a[i];
+                }
+                wartosci[k] = wartosc;
+                wartosc = 0;
+                k++;
+            }
+            for (int i = 0; i < T.Length; i++)
+            {
+                T[i] = 0;
+                Console.Write(T[i]);
+            }
+            for(int i = 0; i < wartosci.Length; i++)
+            {
+                h = wartosci[i] % m;
+                T[h]++;
+            }
+            int Zera = 0;
+            int max = T[0];
+            float liczilosc=0;
+            float liczwartosc=0;
+            for(int i = 0; i < m; i++)
+            {
+                if (T[i] == 0)
+                {
+                    Zera++;
+                }
+                if (T[i] > max)
+                {
+                    max = T[i];
+                }
+                if (T[i] != 0)
+                {
+                    liczilosc++;
+                    liczwartosc = liczwartosc + T[i];
+                }
+            }
+            Console.WriteLine("Ilosc zer:"+Zera);
+            Console.WriteLine("Najwieksza wartosc:" + max);
+            Console.WriteLine("Srednia wartosc niezerowych"+liczwartosc/liczilosc);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace zad2
+{
+    struct nazwiska
+    {
+        private int ilosc;
+        private String nazwisko;
+        public int Ilosc
+        {
+            get
+            {
+                return ilosc;
+            }
+            set
+            {
+                ilosc = value;
+            }
+        }
+        public String Nazwisko
+        {
+            get
+            {
+                return nazwisko;
+            }
+            set
+            {
+                nazwisko = value;
+            }
+        }
+    }
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            nazwiska[] tmp;
+            int m = 47527;
+            int m2 = 100;
+            int[] T = new int[m];
+            int[] T2 = new int[m2];
+            int[] wartosci = new int[30000];
+            string path = @"/home/Dave/Downloads";
+            int k = 0;
+            int wartosc = 0;
+            tmp = File.ReadAllLines(path);
+            int h;
+            foreach (nazwiska a in tmp)
+            {
+                for (int i = 0; i < 30000; i++)
+                {
+                    wartosc += a.Ilosc;
+                }
+                wartosci[k] = wartosc;
+                wartosc = 0;
+                k++;
+            }
+        }
+    }
+}
+
